f6735f3173c1f1c0db80555d3eda7891ea5bca3e
Alex Csiszár <csiszaralex@gmail.com>
2024-12-26 13:30:38 +0100

    :art: Refactors PositionModule to remove PrismaModule import and makes PrismaModule global

--- src/position/position.module.ts
+++ src/position/position.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from 'src/prisma/prisma.module';
 import { PositionController } from './position.controller';
 import { PositionService } from './position.service';
 
 @Module({
   controllers: [PositionController],
   providers: [PositionService],
-  imports: [PrismaModule],
 })
 export class PositionModule {}
